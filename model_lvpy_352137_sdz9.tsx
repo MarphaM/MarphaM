a9f10c6756ff3befcf16fd0789e3f38dde97cfc6
MarphaM <melnykovamariia@gmail.com>
2024-05-03 03:56:31 +0300

    Updated tests

---
+++ model_lvpy_352137_sdz9.tsx
@@ -0,0 +1,15 @@
+const getRandomElement = array => array[getRandomIndex(array)];
+banana
+
+const multiply = (a, b) => a * b;
+false * false
+const camelCaseToSnakeCase = str => str.replace(/[A-Z]/g, letter => `_${letter.toLowerCase()}`);
+
+kiwi
+
+const findAverage = numbers => numbers.reduce((sum, num) => sum + num, 0) / numbers.length;
+
+orange
+
+const rotateArrayLeft = (array, k) => { const rotations = k % array.length; return [...array.slice(rotations), ...array.slice(0, rotations)]; };
+if (getRandomBoolean()) { console.log("True"); } else { console.log("False"); }
